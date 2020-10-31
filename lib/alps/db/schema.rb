0a65db440fa10dbe68c5c167858781da5a2aa9ad
ken <block24block@gmail.com>
2020-10-31 14:47:44 +0000

    index tid and catch_time

--- lib/alps/db/schema.rb
+++ lib/alps/db/schema.rb
@@ -14,6 +14,8 @@ module Alps
             column :catch_time, DateTime
           end
 
+          add_index_unless(db, :points, [:tid, :catch_time])
+
           create_table_unless(db, :src_files) do
             primary_key :id
             column :path, String
@@ -27,6 +29,11 @@ module Alps
 
         def create_table_unless(db, name, &block)
           db.create_table(name, &block) unless db.table_exists?(name)
+        end
+
+        def add_index_unless(db, table, columns)
+          return if db.indexes(table).values.any?{ |cfg| cfg[:columns] == columns }
+          db.add_index(table, columns)
         end
       end
     end
